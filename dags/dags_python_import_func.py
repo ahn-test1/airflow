1ad755b774dba03182b4071a506e2e60f27f1ce3
ahn <akheun@naver.com>
2024-03-14 17:29:11 +0900

    캐치업 수정2

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -7,8 +7,8 @@ from common.common_func import get_sftp
 with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2024, 3, 13, tz="Asia/Seoul"),
-    catchup=True
+    start_date=pendulum.datetime(2024, 3, 12, tz="Asia/Seoul"),
+    catchup=False
 ) as dag:
     
     task_get_sftp = PythonOperator(
