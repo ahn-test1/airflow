dec40152aeb100ee389442141fde12a0d9ed1297
ahn <akheun@naver.com>
2024-03-15 15:50:14 +0900

    수정2

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -8,10 +8,13 @@ with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 3, 12, tz="Asia/Seoul"),
-    catchup=True
+    catchup=False
 ) as dag:
     
     task_get_sftp = PythonOperator(
         task_id = 'task_get_sftp',
         python_callable=get_sftp
     )
+    
+    
+    task_get_sftp
